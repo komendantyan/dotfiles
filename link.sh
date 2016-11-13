56e5cd779930ce87c67e300f71d731ae75738e08
Andrey Komendantyan <komendantyan@gmail.com>
2016-11-14 01:55:56 +0300

    added link.sh

---
+++ link.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+ln -s .dotfiles/bashrc $HOME/.bashrc
+ln -s .dotfiles/bash_aliases $HOME/.bash_aliases
+ln -s .dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s .dotfiles/gitconfig $HOME/.gitconfig
+ln -s .dotfiles/vimrc $HOME/.vimrc
